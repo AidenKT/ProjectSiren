21b667e1f0b4e23c619a78c9e36908dd7c315097
Aiden <33820269+AidenKT@users.noreply.github.com>
2023-01-27 15:56:32 -0800

    Add files via upload

---
+++ AssetExample.js
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import { Text, View, StyleSheet, Image } from 'react-native';
+
+export default function AssetExample() {
+  return (
+    <View style={styles.container}>
+      <Image style={styles.logo} source={require('../assets/snack-icon.png')} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 24,
+  },
+  paragraph: {
+    margin: 24,
+    marginTop: 0,
+    fontSize: 14,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  logo: {
+    height: 48,
+    width: 48,
+  }
+});
